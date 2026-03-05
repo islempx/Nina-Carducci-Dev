a76aaa2a494f14e9acc44fd889c954c5a1d81949
RemadiIslem <islem.remadi69@gmail.com>
2026-03-05 11:55:53 +0100

    fix gallery

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('.gallery').mauGallery({
         columns: {
             xs: 1,
@@ -12,4 +12,23 @@ $(document).ready(function() {
         showTags: true,
         tagsPosition: 'top'
     });
+});*/
+const buttons = document.querySelectorAll(".filters button");
+const images = document.querySelectorAll(".gallery img");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    buttons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    const filter = button.dataset.filter;
+
+    images.forEach(img => {
+      if (filter === "all" || img.dataset.galleryTag === filter) {
+        img.style.display = "block";
+      } else {
+        img.style.display = "none";
+      }
+    });
+  });
 });
